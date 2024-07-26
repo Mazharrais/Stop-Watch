2245543a08047447a10ca6c30eb58345c078a31f
Mazharrais <mazharmajo72@gmail.com>
2024-07-26 22:07:43 +0500

    add code

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 
-let displayTime = document.getElementById("displayTime");
-
-
 let [seconds, minutes, hours] = [0,0,0];
+let displayTime = document.getElementById("displayTime");
+let timer = null;
+
 
 function stopWatch(){
     seconds++;
-    if(seconds == 60){
+    if(seconds == 20){
         seconds = 0;
         minutes++;
         if(minutes == 60){
@@ -14,6 +14,28 @@ function stopWatch(){
             hours++;
         }
     }
+
+   let h = hours < 10 ? "0" + hours : hours;
+   let m = minutes < 10 ? "0" + minutes : minutes;
+   let s = seconds < 10 ? "0" + seconds : seconds;
+
+    displayTime.innerHTML = h +":"+ m +":"+ s;
 }
 
-setInterval(stopWatch,1000)
+function watchStart(){
+    if(timer !== null){
+        clearInterval(timer);
+    }
+    timer = setInterval(stopWatch,1000)
+}
+
+function watchStop(){
+    clearInterval(timer);
+}
+
+
+function watchReset(){
+    clearInterval(timer);
+    [seconds, minutes, hours] = [0,0,0];
+    displayTime.innerHTML = "00:00:00";
+}
